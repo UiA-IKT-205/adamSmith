f718263bf6f1f43f72c2a320df3be732d2979d79
Adam Smith <adamj17@uia.no>
2021-01-29 18:56:10 +0100

    Created the buttons and fixed the bug

--- Pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ Pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import no.uia.ikt205.pomodoro.util.millisecondsToDescriptiveTime
+import no.uia.ikt205.pomodoro.util.minutesToMilliSeconds
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     var timeToCountDownInMs60 = 3600000L
     var timeToCountDownInMs90 = 5400000L
     var timeToCountDownInMs120 = 7200000L
+    val buttonMinuteTimeIncrement = 30L
     val timeTicks = 1000L
 
     // private var stopTimer:Boolean = false //Vil stoppe timeren
@@ -31,49 +33,73 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         startButton = findViewById<Button>(R.id.startCountdownButton)
         startButton.setOnClickListener() {
             startCountDown(it)
         }
 
-        coutdownDisplay = findViewById(R.id.countDownView)
+        coutdownDisplay = findViewById<TextView>(R.id.countDownView)
 
-        //val Timeduration = listOf<Button>(findViewById<Button>(R.id.button30),
+        val timeDurationButtons = listOf<Button>(findViewById<Button>(R.id.button30),
+            findViewById<Button>(R.id.button60),
+            findViewById<Button>(R.id.button90),
+            findViewById<Button>(R.id.button120))
 
-        //Her ifra og videre blir funksjoner til knappene 30,60,90,120 minutter
-        timerB30 = findViewById<Button>(R.id.button30)
-        timerB60 = findViewById<Button>(R.id.button60)
-        timerB90 = findViewById<Button>(R.id.button90)
-        timerB120 = findViewById<Button>(R.id.button120)
+            timeDurationButtons.forEachIndexed { index, button ->
+                button.setOnClickListener(){
+                    if(startButton.isEnabled){
+                            val againCountdown = minutesToMilliSeconds((index+1) * buttonMinuteTimeIncrement)
+                            settimer(againCountdown)
+                        }
 
-        timerB30.setOnClickListener() {
+                }
+            }
+
+        }
+
+        fun settimer (timeToCountDownInMsStartnew:Long) {
+            timeToCountDownInMsStart = timeToCountDownInMsStartnew
+            updateDisplay(timeToCountDownInMsStart)
+        }
+
+        /*Her ifra og videre blir funksjoner til knappene 30,60,90,120 minutter
+          timerB30 = findViewById<Button>(R.id.button30)
+          timerB60 = findViewById<Button>(R.id.button60)
+          timerB90 = findViewById<Button>(R.id.button90)
+          timerB120 = findViewById<Button>(R.id.button120)
+
+          timerB30.setOnClickListener() {
             timeToCountDownInMsStart = timeToCountDownInMs30
         }
-        timerB60.setOnClickListener() {
-            timeToCountDownInMsStart = timeToCountDownInMs60
+          timerB60.setOnClickListener() {
+             timeToCountDownInMsStart = timeToCountDownInMs60
         }
-        timerB90.setOnClickListener() {
+          timerB90.setOnClickListener() {
             timeToCountDownInMsStart = timeToCountDownInMs90
         }
         timerB120.setOnClickListener() {
             timeToCountDownInMsStart = timeToCountDownInMs120
-        }
-    }
+        }*/
+
 
     fun startCountDown(v: View) {
-
         timer = object : CountDownTimer(timeToCountDownInMsStart, timeTicks) {
             override fun onFinish() {
-                Toast.makeText(this@MainActivity, "Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "The job is done man!", Toast.LENGTH_SHORT).show()
             }
-            override fun onTick(millisUntilFinished: Long) {
-                //updateCountDownDisplay(millisUntilFinished)
-                fun updateCountDownDisplay(timeInMs: Long) {
-                    coutdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
 
-                }
+            override fun onTick(millisUntilFinished: Long) {
+                updateDisplay(millisUntilFinished)
             }
+
         }
+        if 
+        v.isEnabled = false
+        timer.start()
+    }
+
+    fun updateDisplay(timeInMs: Long) {
+       coutdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
     }
 }
-      //  timer.start()
